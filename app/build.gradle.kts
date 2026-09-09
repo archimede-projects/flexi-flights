c8f2bfe998631f3b3bf4584f7ae86cecbfc0ae2c
IbnKhaldoun-svg <essalih.idriss.w@gmail.com>
2026-09-09 12:56:03 +0200

    feat: add DataStore and Navigation Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.datastore:datastore-preferences:1.2.1")
+    implementation("androidx.navigation:navigation-compose:2.9.8")
 }
